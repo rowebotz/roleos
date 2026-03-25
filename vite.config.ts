61b2073490b7f45221b6e6c8b6717fdde84e0a56
rowebotz <stephenrowe406@gmail.com>
2026-03-25 15:14:00 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -105,6 +105,7 @@ function reloadTriggerPlugin() {
 export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd());
   return defineConfig({
+    base: '/roleos/',
     plugins: [react(), cloudflare(), watchDependenciesPlugin(), reloadTriggerPlugin()],
     build: {
       minify: true,
